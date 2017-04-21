72945beaaa99ba2657bf58c6cccfc88860307cc0
Vasili Anoshin <anoshin45@gmail.com>
2017-04-21 15:38:03 +0300

    Add DAO interface + implementation
    Add Data Access object class interface + Implementation of this
    interface + inject customerDAOimplementation into Customer Controller
    and get implementation class. Spring will look for DAO impl.

---
+++ src/DAO/CustomerDAO.java
@@ -0,0 +1,9 @@
+package DAO;
+
+import java.util.List;
+
+import entity.Customer;
+
+public interface CustomerDAO {
+	public List<Customer> getCustomers();
+}
--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -1,15 +1,32 @@
 package controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import DAO.CustomerDAO;
+import entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	//need to inject the DAO into Controller
+	//Spring will scan for a component that implements DAO interface 
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
-	public String listcustomers(Model theModel){
+	public String listcustomers(Model theModel){		
+		//get customer from the dao		
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		//add the customer to the model in form <Name>:<Value>
+		theModel.addAttribute("customers", theCustomers );
+						
 		return "list-customers";
 	}
 }
